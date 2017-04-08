e0915cbc5c665147cd13f7df120cb69b8d6f440b
Panagiotis Papasaikas <ppapasaikas@F022M-74DA4E.local>
2017-04-08 20:16:05 +0200

    Added slot for the plot-ready graph

--- griph/R/GriphResult-class.R
+++ griph/R/GriphResult-class.R
@@ -20,7 +20,7 @@ setOldClass("igraph") # make S3 class igraph visible to S4
 #' @slot miscl A length-one numerical value giving the misclassification error.
 #' @slot GRAO An object of class \code{igraph} with c nodes representing cells
 #'     and edges between similar cells.
-#' @slot plot.GRAO An object of class \code{igraph} with c nodes representing cells
+#' @slot plotGRAO An object of class \code{igraph} with c nodes representing cells
 #'     and edges between similar cells. This is a rendering optimized version of GRAO.
 #'     Edges are pruned, vertex attributes for plotting are added and vertices are sampled
 #'     if they exceed the maxG argument.  
@@ -35,7 +35,7 @@ GriphResult <- setClass("GriphResult",
                                                ConfMatrix = "matrix",
                                                miscl = "numeric",
                                                GRAO = "igraph",
-                                               plot.GRAO="igraph")
+                                               plotGRAO="igraph")
 )
 
 #' @param object Instance of class \code{GriphResult}.
