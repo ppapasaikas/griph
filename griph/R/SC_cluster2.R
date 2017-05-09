3a4d34ad5c34f3c3f36d4475b50d6919e244c743
Panagiotis Papasaikas <papapana@f022m-4ee969.local>
2017-05-09 16:34:35 +0200

    Add LargeVis plotting function

--- griph/R/SC_cluster2.R
+++ griph/R/SC_cluster2.R
@@ -923,3 +923,200 @@ plotTsne <- function(gr,
     
     return(res)
 }
+
+
+
+
+
+#' @title Visualize griph result using a LargeVis (Tang et al 2016 ) projection.
+#' 
+#' @description Plot a largeVis projection of the affinity matrix obtained from
+#'     \code{\link{SC_cluster}}, allowing to control coloring.
+#' 
+#' @param gr A \code{griph} result, as returned by \code{\link{SC_cluster}}.
+#' @param fill.type Type of fill color, one of \code{predicted} (predicted class
+#'     labels, default), \code{true} (true class labels, if available), \code{none}
+#'     (no fill color) or \code{custom} (use \code{custom.col}).
+#' @param line.type Type of line color, one of \code{true} (true class labels, if
+#'     available, default), \code{predicted} (predicted class labels), \code{none}
+#'     for no fill color or \code{custom} (use \code{custom.col}).
+#' @param mark.type Type of cell class defnition to mark using polygons,
+#'     one of \code{none} (no polygons, the default), \code{predicted} (draw
+#'     polygons around cells with the same predicted class label), \code{true}
+#'     (polygons around cells with the same true class label, if available) or
+#'     \code{custom} (polygons around cells with the same \code{custom.class}).
+#' @param fill.col Character scalar with a \code{\link{RColorBrewer}} color palette name
+#'     or color vector defining the palette to use for vertex fill coloring.
+#' @param line.col Character scalar with a \code{\link{RColorBrewer}} color palette name
+#'     or color vector defining the palette to use for vertex outline coloring.
+#' @param mark.col Character scalar with a \code{\link{RColorBrewer}} color palette name
+#'     or color vector defining the palette to use for cell class polygon marking.
+#' @param custom.class Factor, character or numberic vector of the same length or
+#'     with names corresponding to names(gr$MEMB) to use for custom cell classification
+#'     (used if \code{fill.type} and/or \code{line.type} is set to "custom").
+#' @param seed Random number seed to make largeVis projection deterministic.
+#' @param fsuffix A suffix added to the file names of output plots. If not given
+#'     it will use a random 5 character string. Ignored if \code{image.format} is \code{NULL}.
+#' @param image.format Specifies the format of the created image. Currently supported are
+#'     \code{\link{pdf}}, \code{\link{png}} or \code{NA}. If \code{NA} (the default), the plot
+#'     is rendered on the currently opened plotting device.
+#' @param quiet If \code{TRUE}, do not report on progress.
+#' @param ... additional arguments passed to \code{largeVis}
+#' 
+#' @return Invisible the results of the largeVis projection.
+#' 
+#' @seealso \code{largeVis} used to calculate the largeVis projection.
+plotLVis <- function(gr,
+                     fill.type=c("predicted","true","none","custom"),
+                     line.type=c("true","predicted","none","custom"),
+                     mark.type=c("none","predicted","true","custom"),
+                     fill.col="Spectral",
+                     line.col="Dark2",
+                     mark.col="Pastel1",
+                     custom.class=factor(rep(1, length(gr$MEMB))),
+                     seed=91919,
+                     fsuffix=RandString(), image.format=NA,
+                     quiet=FALSE, ...) {
+    if(! is.element("largeVis", utils::installed.packages()[,1]))
+        stop('"plotLvis" requires the "largeVis" package. Please install it with:\n\t',
+             'install.packages("largeVis")')
+    if(! is.element("Matrix", utils::installed.packages()[,1]))
+        stop('"plotLvis" requires the "Matrix" package. Please install it with:\n\t',
+             'install.packages("Matrix")')
+    
+    # get varaibles from gr
+    MEMB <- gr$MEMB
+    MEMB.true <- gr$MEMB.true
+    csize <- table(MEMB)
+    
+    # digest arguments
+    fill.type <- match.arg(fill.type)
+    line.type <- match.arg(line.type)
+    mark.type <- match.arg(mark.type)
+    if(is.null(names(custom.class))) {
+        stopifnot(length(custom.class)==length(MEMB))
+        names(custom.class) <- names(MEMB)
+    }
+    if(length(fill.col)==1) {
+        stopifnot(fill.col %in% rownames(RColorBrewer::brewer.pal.info))
+        fill.col <- RColorBrewer::brewer.pal(RColorBrewer::brewer.pal.info[fill.col, "maxcolors"], fill.col)
+    }
+    if(length(line.col)==1) {
+        stopifnot(line.col %in% rownames(RColorBrewer::brewer.pal.info))
+        line.col <- RColorBrewer::brewer.pal(RColorBrewer::brewer.pal.info[line.col, "maxcolors"], line.col)
+    }
+    if(length(mark.col)==1) {
+        stopifnot(mark.col %in% rownames(RColorBrewer::brewer.pal.info))
+        mark.col <- RColorBrewer::brewer.pal(RColorBrewer::brewer.pal.info[mark.col, "maxcolors"], mark.col)
+    }
+    
+    # global plotting paramterers
+    my.pch <- 21L # should be in 21:25
+    my.pt.cex <- 2.5
+    my.pt.lwd <- if(line.type == "none") 1.0 else 2.5
+    
+    # get largeVis projection
+    if(!quiet)
+        message("Computing largeVis projection...")
+    set.seed(seed = seed)
+    res <- largeVis::projectKNNs(Matrix::Matrix(gr$DISTM), sgd_batches=0.1, M=3, gamma=32, alpha=0.25, useDegree=TRUE, ...)
+    
+    # get colors
+    class.pred <- factor(MEMB, levels=sort(as.numeric(unique(MEMB))))
+    class.true <- factor(MEMB.true, levels=unique(MEMB.true))
+    class.none <- factor(rep(NA, length(MEMB)))
+    class.custom <- factor(custom.class, levels=unique(custom.class))
+    
+    class.fill <- switch(fill.type,
+                         predicted=class.pred,
+                         true=class.true,
+                         none=class.none,
+                         custom=class.custom)
+    fillColorPalette <- if(nlevels(class.fill) > length(fill.col)) grDevices::colorRampPalette(fill.col)(nlevels(class.fill)) else fill.col
+    fillColor <- fillColorPalette[as.numeric(class.fill)]
+    
+    class.line <- switch(line.type,
+                         predicted=class.pred,
+                         true=class.true,
+                         none=class.none,
+                         custom=class.custom)
+    lineColorPalette <- if(nlevels(class.line) > length(line.col)) grDevices::colorRampPalette(line.col)(nlevels(class.line)) else line.col
+    lineColor <- lineColorPalette[as.numeric(class.line)]
+    
+    class.mark <- switch(mark.type,
+                         predicted=class.pred,
+                         true=class.true,
+                         none=class.none,
+                         custom=class.custom)
+    markElements <- split(seq_along(class.mark), class.mark)
+    markColor <- if(nlevels(class.mark) > length(mark.col)) grDevices::colorRampPalette(mark.col)(nlevels(class.mark)) else mark.col[1:nlevels(class.mark)]
+    
+    # open output file
+    if(!is.na(image.format)) {
+        if(image.format=='pdf') {
+            fname <- paste('Lvis_',fsuffix,'.pdf',sep="")
+            pdf(file = fname, width = 12, height = 10)
+        } else if (image.format=="png") {
+            fname <- paste('Lvis_',fsuffix,'.png',sep="")
+            png(filename = fname, width = 12, height = 10, units = "in", res = 300)   
+        }
+        if(!quiet)
+            message("\tsaving LargeVis plot to ",fname)
+    }
+    
+    # setup plot coordinate system
+    par(mar=c(5.1, 4.1, 4.1, 14.1), xpd=TRUE)
+    plot(t(res)[,1], t(res)[,2], type = "n", axes=FALSE, xlab="", ylab="")
+    
+    # add mark polygons
+    if(mark.type != "none") {
+        for(j in which(lengths(markElements) > 0)) {
+            xy <- t(res)[markElements[[j]], , drop = FALSE]
+            off <- par("cxy")[2]*1
+            pp <- rbind(xy,
+                        cbind(xy[, 1] - off, xy[, 2]),
+                        cbind(xy[, 1] + off, xy[, 2]),
+                        cbind(xy[, 1], xy[, 2] - off), 
+                        cbind(xy[, 1], xy[, 2] + off))
+            cl <- igraph::convex_hull(pp)
+            graphics::xspline(cl$rescoords, shape = 0.5, open = FALSE,
+                              col = paste0(markColor[j], "66"),
+                              border = adjust.color(markColor[j], 0.5))
+            
+        }
+    }
+    
+    # add cells
+    points(t(res), col = if(line.type=="none") "black" else lineColor,
+           bg = fillColor, pch = my.pch, lwd = my.pt.lwd, cex=my.pt.cex)
+    
+    # add legend(s)
+    if(mark.type != "none") {
+        lgd <- legend(x = par("usr")[2]+12*par("cxy")[1], y = par("usr")[4], xjust = 1, yjust = 1, bty = "n",
+                      cex = 1, fill = paste0(markColor, "66"), title = mark.type, legend = levels(class.mark))
+    } else {
+        lgd <- list(rect=list(left=par("usr")[2]+12*par("cxy")[1]))
+    }
+    if(fill.type != "none" && nlevels(class.fill) > 0) {
+        lgd <- legend(x = lgd$rect$left, y = par("usr")[4], xjust = 1, yjust = 1, bty = "n",
+                      pch = my.pch, pt.lwd = my.pt.lwd, cex = 1, pt.cex = my.pt.cex,
+                      col = if(line.type=="none") "black" else "white", pt.bg = fillColorPalette,
+                      title = fill.type, legend = levels(class.fill))
+    }
+    if(line.type != "none" && nlevels(class.line) > 0) {
+        legend(x = lgd$rect$left, y = par("usr")[4], xjust = 1, yjust = 1, bty = "n",
+               pch = my.pch, pt.lwd = my.pt.lwd, cex = 1, pt.cex = my.pt.cex,
+               col = lineColorPalette, pt.bg = "white",
+               title = line.type, legend = levels(class.line))
+    }
+    
+    # close output file
+    if(!is.na(image.format))
+        dev.off()
+    
+    return(res)
+}
+
+
+
+
