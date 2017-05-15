312e717dc77be4e1dc7364a0cdfa0dd848d8dc09
mbstadler <stadler.michael@gmail.com>
2017-05-15 11:08:51 +0200

    finalize merge of former Testing into master

--- prepare_build_install.R
+++ prepare_build_install.R
@@ -145,6 +145,10 @@ griph:::classError(res$MEMB, cc)
 griph:::classError(res$MEMB, c("G1.S" = "G1", "S" = "S", "G2" = "G2M",
                                "G2.M" = "G2M", "M.G1" = "G1")[as.character(cc)])
 
+res <- griph_cluster(M, ClassAssignment = label, plotG = FALSE)
+res$miscl
+
+
 g <- plotGraph(res)
 g <- plotGraph(res, fill.type = "custom", custom.class = cc, fill.col = "Set1")
 g <- plotGraph(res, mark.type = "true")
