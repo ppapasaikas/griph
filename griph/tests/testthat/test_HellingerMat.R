14a60a851c96cac96e743841f3d2261f75050eaa
mbstadler <stadler.michael@gmail.com>
2017-06-12 12:10:57 +0200

    remove test for removed c++ functions, added tests for PCanberraMat and PHellingerMat

--- griph/tests/testthat/test_HellingerMat.R
+++
@@ -1,6 +0,0 @@
-test_that("HellingerMat works properly", {
-    x <- matrix(0:8, nrow = 3)
-    expect_is(HellingerMat(x), "matrix")
-    expect_equal(sum(HellingerMat(x)), 1.617188192, tolerance = 1.0e-8)
-    expect_equal(sum(is.finite(HellingerMat(x - 3))), 9)
-})
---
+++ griph/tests/testthat/test_dists.R
@@ -0,0 +1,17 @@
+test_that("PHellingerMat works properly", {
+    x <- matrix(0:8, nrow = 3)
+    res1 <- PHellingerMat(x, x)
+    res2 <- PHellingerMat(x - 3, x - 3)
+    expect_is(res1, "matrix")
+    expect_equal(sum(res1), 1.617188192, tolerance = 1.0e-8)
+    expect_equal(sum(is.finite(res2)), 9)
+})
+
+test_that("PCanberraMat works properly", {
+    x <- matrix(0:8, nrow = 3)
+    res1 <- PCanberraMat(x, x)
+    res2 <- PCanberraMat(x - 3, x - 3)
+    expect_is(res1, "matrix")
+    expect_equal(sum(res1), 10.4308025308, tolerance = 1.0e-8)
+    expect_equal(sum(is.finite(res2)), 7)
+})
