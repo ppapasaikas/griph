05007cd697dc075204cc7b404171913f05f64e62
mbstadler <stadler.michael@gmail.com>
2017-06-12 09:18:00 +0200

    added unit tests for keep.mknn, sparse.cor and psparse.cor

--- griph/tests/testthat/test_Helper_Functions.R
+++ griph/tests/testthat/test_Helper_Functions.R
@@ -8,12 +8,43 @@ test_that("get.knn works properly", {
     expect_equal(res1[-1,], res2)
 })
 
+test_that("keep.mknn works properly", {
+    set.seed(0)
+    M <- as(matrix(rnorm(100), nrow=10), "sparseMatrix")
+    res1 <- keep.mknn(M, 3)
+    res2 <- keep.mknn(M, 4)
+    expect_is(res1, "Matrix")
+    expect_equal(sum(res1 > 0), 12)
+    expect_true(all(which(as.logical(res1 > 0)) %in% which(as.logical(res2 > 0))))
+})
+
 test_that("sparsify works properly", {
     set.seed(0)
     M <- as(matrix(abs(rnorm(100)), nrow=10), "sparseMatrix")
     res <- sparsify(M, 0.5)
     expect_is(res, "Matrix")
     expect_equal(sum(res > 0), 50)
+})
+
+test_that("sparse.cor works properly", {
+    set.seed(7)
+    M <- as(matrix(abs(rnorm(200)), nrow=10), "sparseMatrix")
+    res1 <- sparse.cor(M)
+    res2 <- cor(as.matrix(M))
+    expect_is(res1, "matrix")
+    expect_equal(dim(res1), dim(res2))
+    expect_true(all(abs(res1-res2) < 1e-6))
+})
+
+test_that("psparse.cor works properly", {
+    set.seed(3)
+    M1 <- as(matrix(abs(rnorm(50)), nrow=10), "sparseMatrix")
+    M2 <- as(matrix(abs(rnorm(50)), nrow=10), "sparseMatrix")
+    res1 <- psparse.cor(M1, M2)
+    res2 <- cor(as.matrix(M1), as.matrix(M2))
+    expect_is(res1, "matrix")
+    expect_equal(dim(res1), dim(res2))
+    expect_true(all(abs(res1-res2) < 1e-6))
 })
 
 test_that("gg_color_hue works properly", {
